273ed05407164ad31bd45331d376db9864afa4dc
ThakurSaAbhay <thakursaabhay@gmail.com>
2023-05-18 12:34:47 +0530

    Initial commit

---
+++ app/src/main/java/com/thakursa/nerdo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.thakursa.nerdo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/thakursa/nerdo/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.thakursa.nerdo
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.view.animation.AnimationUtils.loadAnimation
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val logo=findViewById<ImageView>(R.id.logo)
+        setContentView(R.layout.activity_splash)
+//        val animation: Animation = AnimationUtils.loadAnimation(
+//            this , R.anim.zoomanim)
+//        logo.startAnimation(animation)
+//        YoYo.with(Techniques.RubberBand).duration(700).repeat(1).playOn(logo)
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            var item = Intent(this, LoginActivity::class.java)
+            startActivity(item)
+            finish()
+        },5000)
+
+    }
+}
